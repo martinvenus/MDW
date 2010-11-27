6b0802e2e0078135b67899af5090684c71ce9f4a
Martin Venuš <martin.venus@gmail.com>
2010-11-27 21:04:31 +0100

    Upraven RESTClient

--- app/FrontModule/presenters/RestClientPresenter.php
+++ app/FrontModule/presenters/RestClientPresenter.php
@@ -17,17 +17,23 @@
 class Front_RestClientPresenter extends Front_BasePresenter {
 
     function actionDefault() {
-        $data = '<?xml version="1.0" encoding="UTF-8">
-<project>
-    <name>TEST</project>
-    <type>TEST</type>
-    <tags>TEST</tags>
-    <description>TEST</description>
+        $data = '<project>
+    <name>Zda_se_ze_ani_projekt_s_otaznikem_NEFUNGUJE</name>
+    <type>service</type>
+    <tags>NEFUNGUJE</tags>
+    <description>kdde se to zobrazuje?</description>
 </project>';
 
-        $url = 'http://fit-mdw-ws10-102-7.appspot.com/rest/projects?user=martin.venus@gmail.com';
-        $test = RestClientModel::post($url, $data, null, null, "application/xml");
+        $bribe='<bribe>
+    <state>accepted</state>
+    <oficialNote>{acception/rejection clarification}</oficialNote>
+</bribe>';
 
+        $url = 'http://fit-mdw-ws10-102-7.appspot.com/rest/projects/test?user=martin.venus@gmail.com';
+        //$test = RestClientModel::post($url, $data, null, null, "application/xml");
+        //$test = RestClientModel::delete('http://fit-mdw-ws10-102-7.appspot.com/rest/projects/martin+-+test');
+
+        $test = RestClientModel::put('http://fit-mdw-ws10-102-7.appspot.com/rest/bribes/ahJmaXQtbWR3LXdzMTAtMTAyLTdyQwsSB1Byb2plY3QiK3pkYV9zZV96ZV9hbmlfcHJvamVrdF9zX290YXpuaWtlbV9uZWZ1bmd1amUMCxIFQnJpYmUYAQw', $bribe);
         echo $test->getResponse();
         var_dump($test->getResponseCode());
 //var_dump($test->getResponseMessage());
--- app/models/RestModel.php
+++ app/models/RestModel.php
@@ -15,12 +15,42 @@
  *
  */
 class RestModel extends BaseModel {
+    /*
+     * Vytvoří tiket ve vzdáleném API
+     * @action typ akce (0=vytvoření, 1=změna)
+     * @param ticketId
+     * @param subject předmět tiketu
+     * @param description popis tiketu
+     * @return návratová hodnota
+     */
+
+    public static function createTicketInRemoteAPI($ticketId, $subject, $description, $action = 0) {
+        $data = '<project>
+    <name>' . $ticketId . '</name>
+    <type>service</type>
+    <tags>' . $subject . '</tags>
+    <description>' . $subject . ': ' . $description . '</description>
+</project>';
+
+        $url = 'http://fit-mdw-ws10-102-7.appspot.com/rest/projects/test?user=martin.venus@gmail.com';
+
+        if ($action == 0) {
+            $api = RestClientModel::post($url, $data, null, null, "application/xml");
+        } elseif ($action == 1) {
+            $api = RestClientModel::put($url, $data, null, null, "application/xml");
+        } else {
+            throw new Exception('Invalid type of action!');
+        }
+
+        return $api;
+    }
 
     /*
      * Metoda ověří, zda přijatá data jsou požadovaného typu
      * @param requiredContentType požadovaný typ dokumentu
      * @param givenContentType přijatý typ dokumentu
      */
+
     public static function verifyContentType($requiredContentType, $givenContentType) {
         trim($requiredContentType);
         trim($givenContentType);
@@ -36,6 +66,7 @@ class RestModel extends BaseModel {
      * Metoda ověří, zda je zadaný klíč platný
      * @param key API klíč
      */
+
     public static function verifyAPIkey($key) {
         $result = dibi::query('SELECT id FROM api WHERE `key`=%s AND active = 1', $key);
         $count = count($result);
