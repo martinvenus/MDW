3108dca41c23576df33f87dc7af2ca576b3871b8
libaljar <Jaroslav Lbal@.(none)>
2010-11-28 11:56:28 +0100

    Rozpracovana cestovka - templaty

---
+++ app/AdminModule/presenters/HolidayPresenter.php
@@ -0,0 +1,89 @@
+<?php
+
+/**
+ * Request Tracking System
+ * MI-MDW at CZECH TECHNICAL UNIVERSITY IN PRAGUE
+ *
+ * @copyright  Copyright (c) 2010
+ * @package    RTS
+ * @author     Andrey Chervinka, Jaroslav Líbal, Martin Venuš
+ */
+
+/**
+ *
+ * Ticket managment
+ *
+ */
+class Admin_HolidayPresenter extends Admin_BasePresenter {
+
+    /** @var Form */
+    protected $form;
+
+    public function startup() {
+
+        parent::startup();
+        
+    }
+
+    /*
+     * Výchozí akce presenteru
+     */
+
+    public function actionDefault() {
+        
+    }
+
+    /**
+     *
+     * Pridani tiketu do seznamu uplatku (vyuziti API jineho tymu)
+     * ID projektu ve vzdalenem systemu si ulozim do databaze
+     *
+     */
+    public static function orderZajezd($id) {
+        $data = '<Objednavka>
+    <caId>MiDWa</caId>
+    <cena>6000</cena>
+    <datumVytvoreni>2010-11-20T17:59:20.002Z</datumVytvoreni>
+    <osoby>
+      <jmeno>' . $this->user->getidentity()->name . '</jmeno>
+      <prijmeni>' . $this->user->getidentity()->surname . '</prijmeni>
+      <vek>0</vek>
+    </osoby>
+    <stav>prijata</stav>
+    <uzivatelId>jardakiss@gmail.com</uzivatelId>
+    <zajezdId>' . $id . '</zajezdId>
+  </Objednavka>';
+
+        $req = RestClientModel::post('http://fit-mdw-ws10-103-5.appspot.com/rest/Objednavky', $data, null, null, 'application/xml');
+
+        if ($req->getResponseCode() == 200) {
+
+            $xmlDOM = new DOMDocument();
+            $response = $req->getResponse();
+
+            $response = trim($response);
+
+            $xmlDOM->loadXML($response);
+
+            $xml = simplexml_import_dom($xmlDOM);
+
+            $objednavkaId = (String) $xml->objednavkaId;
+
+            // TODO: Az opravi API dodelat vlozeni do DB ticketBribe
+
+            echo $objednavkaId;
+
+
+
+//
+//            try {
+//                TicketsModel::addBribe($detaily['ticketId'], $projectId);
+//                dibi::query('COMMIT');
+//            } catch (Exception $e) {
+//                dibi::query('ROLLBACK');
+//                Debug::processException($e);
+//            }
+        }
+    }
+
+}
