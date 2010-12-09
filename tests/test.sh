e809a8d40b4b3d4cb7d72bcf033f9c81ca5953f9
Martin Venuš <martin.venus@gmail.com>
2010-12-09 10:53:35 +0100

    Testovani

--- tests/test.sh
+++ tests/test.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
-for N in 10 20 50 100 150 200 300 400 500; 
+
+if [ -z $1 ]; then
+    echo "Nebyla zadana adresa serveru, ze ktereho se bude provadet test! Adresu je nutne zadat jako prvni argument pri spusteni skriptu."
+    exit 1
+fi
+
+if [ -z $2 ]; then
+    echo "Nebyla zadana adresa API serveru! Adresu je nutne zadat jako druhy argument pri spusteni skriptu."
+    exit 1
+fi
+
+for N in 10 20 50 100; 
 do
-  LOG="result_"$N".log"
-  ab -c $N -n 1000 http://localhost/MDW/document_root/front/rest/get-benchmark/?server=localhost/MDW/document_root > $LOG
+  LOG="log/result_"$N".log"
+  ab -c $N -n 1000 http://$1/front/rest/get-benchmark/?server=$2 > $LOG
   sleep 5s
 done
 
